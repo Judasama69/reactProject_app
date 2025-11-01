f6cdc76d3eb96ca09a3d85aac67480ec62b18f1b
Judasama69 <kenjiepen14@gmail.com>
2025-11-01 18:08:52 +0800

    commit

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,11 +1,16 @@
 import Student from './Student'
 import Header from './Header'
+import Homepage from './Homepage'
+import Button from './Button'
 
 function App() {
   return(
     <>
+      <Button />
+      <Homepage />
       <Header />
       <Student name = "Kenjie" status = "Grinding"/>
+      
     </>
   );
   
--- my-react-app/src/Button.jsx
+++ my-react-app/src/Button.jsx
@@ -1,17 +1,8 @@
 
-function Button(){
-
-    const styles = {   
-    backgroundColor: "hsl(200, 100%, 50%)",
-    color: "white",
-    padding: "10px 20px",
-    borderRadius: "5px",
-    border: "none",
-    cursor: "pointer",
-
-    }
-
-    return(<button style = {styles}>Click Me</button>);
+function Button() {
+    return (
+        <button>Click Me</button>
+    )
 }
 
 export default Button
